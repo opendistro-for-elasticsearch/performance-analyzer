f9a65a78d9626c856404f751875291c38394afd4
Balaji Kannan <kbalaji@amazon.com>
2019-10-24 12:57:08 -0700

    * Changes include support for https reader endpoints * Minor fixes and additions to unit tests

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/http_action/config/PerformanceAnalyzerResourceProvider.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/http_action/config/PerformanceAnalyzerResourceProvider.java
@@ -16,13 +16,24 @@
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.http_action.config;
 
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.config.PluginSettings;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.node.NodeClient;
@@ -35,22 +46,57 @@ import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestResponse;
 import org.elasticsearch.rest.RestStatus;
 
-
 public class PerformanceAnalyzerResourceProvider extends BaseRestHandler {
   private static final Logger LOG = LogManager.getLogger(PerformanceAnalyzerResourceProvider.class);
-  private static PerformanceAnalyzerResourceProvider instance = null;
+
   private static final int HTTP_CLIENT_CONNECTION_TIMEOUT_MILLIS = 200;
   private static final String AGENT_PATH = "/_opendistro/_performanceanalyzer/_agent/";
   private static final String DEFAULT_PORT_NUMBER = "9650";
-  private final String portNumber;
-  private static final String RCA = "rca";
-  private static final String METRICS = "metrics";
+
+  private String portNumber;
+  private final boolean isHttpsEnabled;
+  private static Set<String> SUPPORTED_REDIRECTIONS = ImmutableSet.of("rca", "metrics");
 
   @Inject
   public PerformanceAnalyzerResourceProvider(Settings settings, RestController controller) {
     super(settings);
     controller.registerHandler(org.elasticsearch.rest.RestRequest.Method.GET, AGENT_PATH + "{redirectEndpoint}", this);
-    portNumber = PluginSettings.instance().getSettingValue("webservice-listener-port", DEFAULT_PORT_NUMBER);
+    PluginSettings pluginSettings = PluginSettings.instance();
+    portNumber = pluginSettings.getSettingValue("webservice-listener-port", DEFAULT_PORT_NUMBER);
+    isHttpsEnabled = pluginSettings.getHttpsEnabled();
+
+    if (isHttpsEnabled) {
+      // skip host name verification
+      // Create a trust manager that does not validate certificate chains
+      TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+          return null;
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+      }
+      };
+
+      // Install the all-trusting trust manager
+      try {
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+      } catch (NoSuchAlgorithmException e) {
+        LOG.error("Error encountered while initializing SSLContext", e);
+      } catch (KeyManagementException e) {
+        LOG.error("Error encountered while initializing SSLContext", e);
+      }
+
+      // Create all-trusting host name verifier
+      HostnameVerifier allHostsValid = (hostname, session) -> true;
+      // Install the all-trusting host verifier
+      HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    }
   }
 
   public String getName() {
@@ -69,11 +115,10 @@ public class PerformanceAnalyzerResourceProvider extends BaseRestHandler {
         channel.sendResponse(finalResponse);
       };
     } else {
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setConnectTimeout(HTTP_CLIENT_CONNECTION_TIMEOUT_MILLIS);
-
+      HttpURLConnection httpURLConnection = isHttpsEnabled ? createHttpsURLConnection(url) :
+              createHttpURLConnection(url);
       //Build Response in buffer
-      try ( BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
         while ((inputLine = in.readLine()) != null) {
           response.append(inputLine);
         }
@@ -81,7 +126,7 @@ public class PerformanceAnalyzerResourceProvider extends BaseRestHandler {
       } catch (Exception ex) {
         LOG.error("Error receiving response for Request Uri {} - {}", request.uri(), ex);
         return channel -> {
-          channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR,"Something went wrong"));
+          channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, "Something went wrong"));
         };
       }
 
@@ -89,7 +134,7 @@ public class PerformanceAnalyzerResourceProvider extends BaseRestHandler {
         try {
           RestResponse finalResponse = new BytesRestResponse(RestStatus.OK, String.valueOf(response));
           LOG.debug("finalResponse: {}", finalResponse);
-          Map<String, List<String>> map = conn.getHeaderFields();
+          Map<String, List<String>> map = httpURLConnection.getHeaderFields();
           for (Map.Entry<String, List<String>> entry : map.entrySet()) {
             finalResponse.addHeader(entry.getKey(), entry.getValue().toString());
           }
@@ -97,38 +142,50 @@ public class PerformanceAnalyzerResourceProvider extends BaseRestHandler {
           channel.sendResponse(finalResponse);
         } catch (Exception ex) {
           LOG.error("Error sending response", ex);
-          channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR,"Something went wrong"));
+          channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, "Something went wrong"));
         }
       };
     }
   }
 
+  private HttpURLConnection createHttpsURLConnection(URL url) throws IOException {
+    HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+    httpsURLConnection.setConnectTimeout(HTTP_CLIENT_CONNECTION_TIMEOUT_MILLIS);
+    return httpsURLConnection;
+  }
+
+  private HttpURLConnection createHttpURLConnection(URL url) throws IOException {
+    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+    httpURLConnection.setConnectTimeout(HTTP_CLIENT_CONNECTION_TIMEOUT_MILLIS);
+    return httpURLConnection;
+  }
+
+  @VisibleForTesting
+  void setPortNumber(String portNumber) {
+    this.portNumber = portNumber;
+  }
+
   /**
    * Get Agent URI mapping
+   *
    * @param request : RestRequest as input with valid URI
    * @return URI of target path
    * @throws IOException
    */
-  public URL getAgentUri(RestRequest request) throws IOException{
+  public URL getAgentUri(RestRequest request) throws IOException {
     String redirectEndpoint = request.param("redirectEndpoint");
-    String uri = "";
-    String redirectBasePath = "http://localhost:" + portNumber + "/_opendistro/_performanceanalyzer/";
+    String urlScheme = isHttpsEnabled ? "https://" : "http://";
+    String redirectBasePath = urlScheme + "localhost:" + portNumber + "/_opendistro/_performanceanalyzer/";
     // Need to register all params in ES request else es throws illegal_argument_exception
-    for (String key : request.params().keySet()){
+    for (String key : request.params().keySet()) {
       request.param(key);
     }
 
     // Add Handler whenever add new redirectAgent path
-    switch (redirectEndpoint){
-      case METRICS:
-      case RCA:
-        uri = String.format(redirectBasePath + request.uri().split(AGENT_PATH)[1]);
-        break;
-
-      default:
-        LOG.error("Endpoint path Unexpected for: {}", redirectEndpoint);
-        return null;
+    if (SUPPORTED_REDIRECTIONS.contains(redirectEndpoint)) {
+      String uri = String.format(redirectBasePath + request.uri().split(AGENT_PATH)[1]);
+      return new URL(uri);
     }
-    return new URL(uri);
+    return null;
   }
 }
