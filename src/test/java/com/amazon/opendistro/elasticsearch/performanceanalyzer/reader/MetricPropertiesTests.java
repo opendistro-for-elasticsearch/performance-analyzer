ad2bb4cd77c016ef1ad4874fe6ab8b2d60ef4f1a
Aditya Jindal <aditjind@amazon.com>
2020-05-15 06:03:53 +0530

    Adding Shard Size Metric as a part of Node Stats

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/MetricPropertiesTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/MetricPropertiesTests.java
@@ -138,7 +138,7 @@ public class MetricPropertiesTests extends AbstractReaderTests {
             long storedFieldsMemory, long termVectorsMemory,
             long normsMemory, long pointsMemory, long docValuesMemory,
             long indexWriterMemory, long versionMapMemory,
-            long bitsetMemory) {
+            long bitsetMemory, long shardSizeInBytes) {
         return createShardStatMetrics(
                 indexingThrottleTime,
                 queryCacheHitCount, queryCacheMissCount,
@@ -153,7 +153,7 @@ public class MetricPropertiesTests extends AbstractReaderTests {
                 normsMemory, pointsMemory, docValuesMemory,
                 indexWriterMemory, versionMapMemory,
                 bitsetMemory,
-                FailureCondition.NONE);
+                FailureCondition.NONE, shardSizeInBytes);
     }
 
     @Test
@@ -184,12 +184,12 @@ public class MetricPropertiesTests extends AbstractReaderTests {
         write(moviesShard0, false, PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds(),
                 createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                         0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 2L, 8145L, 6565L,
-                        672L, 0L, 384L, 28L, 496L, 0L, 0L, 0L));
+                        672L, 0L, 384L, 28L, 496L, 0L, 0L, 0L, 0L));
 
         write(moviesShard1, false, PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds(),
                 createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                         0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 2L, 8019L, 6445L,
-                        664L, 0L, 384L, 6L, 520L, 0L, 0L, 0L));
+                        664L, 0L, 384L, 6L, 520L, 0L, 0L, 0L, 0L));
 
         File taxisShard0 = temporaryFolder
                 .newFile(createRelativePath(taxisIndexeRelativePath, "0"));
@@ -197,7 +197,7 @@ public class MetricPropertiesTests extends AbstractReaderTests {
         write(taxisShard0, false, PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds(),
                 createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                         0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
-                        0L, 0L, 0L, 0L, 0L, 0L, 0L));
+                        0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L));
 
         long lastSnapTimestamp = System.currentTimeMillis() - 1000;
 
@@ -328,20 +328,20 @@ public class MetricPropertiesTests extends AbstractReaderTests {
                         createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                                 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 2L,
                                 8145L, 6565L, 672L, 0L, 384L, 28L, 496L, 0L, 0L,
-                                0L));
+                                0L, 0L));
             } else if (condition == FailureCondition.INVALID_JSON_METRIC) {
                 write(moviesShard0, false, PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds(),
                         createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                                 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 2L,
                                 8145L, 6565L, 672L, 0L, 384L, 28L, 496L, 0L, 0L,
-                                0L, condition));
+                                0L, condition, 0L));
             }
             else {
                 write(moviesShard0, false, PerformanceAnalyzerMetrics.getJsonCurrentMilliSeconds(),
                         createShardStatMetrics(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
                                 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 2L,
                                 8145L, 6565L, 672L, 0L, 384L, 28L, 496L, 0L, 0L,
-                                0L));
+                                0L, 0L));
             }
         }
 
