e8753dc2bebd1e4e921c0c709ca8ec5b7e306a19
Aditya Jindal <aditjind@amazon.com>
2020-10-22 14:32:23 -0700

    Test Commit

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/PerformanceAnalyzerPlugin.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/PerformanceAnalyzerPlugin.java
@@ -290,6 +290,8 @@ public final class PerformanceAnalyzerPlugin extends Plugin implements ActionPlu
         return Arrays.asList(performanceanalyzerConfigAction, paClusterConfigAction, performanceAnalyzerRp, paOverridesConfigClusterAction);
     }
 
+
+
     @Override
     @SuppressWarnings("checkstyle:parameternumber")
     public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
@@ -318,6 +320,10 @@ public final class PerformanceAnalyzerPlugin extends Plugin implements ActionPlu
         ESResources.INSTANCE.setCircuitBreakerService(circuitBreakerService);
         return Collections.emptyMap();
     }
+
+
+
+
 
     /**
      * Returns a list of additional {@link Setting} definitions for this plugin.
