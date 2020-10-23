921455149a339c0fbe4776f624de3890e511fe10
Aditya Jindal <aditjind@amazon.com>
2020-10-22 18:11:35 -0700

    Checking Tests

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/http_action/config/PerformanceAnalyzerResourceProviderTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/http_action/config/PerformanceAnalyzerResourceProviderTests.java
@@ -43,7 +43,7 @@ import static org.mockito.MockitoAnnotations.initMocks;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({PluginSettings.class})
 @SuppressStaticInitializationFor({"PluginSettings"})
-public class PerformanceAnalyzerResourceProviderTest {
+public class PerformanceAnalyzerResourceProviderTests {
   @Mock
   RestController mockRestController;
   PerformanceAnalyzerResourceProvider performanceAnalyzerRp;
