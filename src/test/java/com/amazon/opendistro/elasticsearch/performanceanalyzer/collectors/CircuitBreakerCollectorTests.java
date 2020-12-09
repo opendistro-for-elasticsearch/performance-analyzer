c9190f94dbc907be816668ec1937f216774ed7fe
Yu Sun <11154022+yu-sun-77@users.noreply.github.com>
2020-12-09 11:05:20 -0800

    Improve Test coverage up to 48% (#255)
    * add UT for CircuitBreakerCollectorTests
    
    * add UT for PerformanceAnalyzerActionFilterTests
    
    * add UT for PerformanceAnalyzerPluginTests
    
    * add license header
    
    * change min jacoco coverage to 0.48

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/CircuitBreakerCollectorTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/CircuitBreakerCollectorTests.java
@@ -15,42 +15,66 @@
 
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors;
 
-import org.junit.Ignore;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.ESResources;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors.CircuitBreakerCollector.CircuitBreakerStatus;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsConfiguration;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.util.TestUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paranamer.ParanamerModule;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.elasticsearch.common.breaker.CircuitBreaker;
+import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.amazon.opendistro.elasticsearch.performanceanalyzer.CustomMetricsLocationTestBase;
-import com.amazon.opendistro.elasticsearch.performanceanalyzer.config.PluginSettings;
-import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsConfiguration;
-import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
-import static org.junit.Assert.assertEquals;
-@Ignore
-public class CircuitBreakerCollectorTests extends CustomMetricsLocationTestBase {
+public class CircuitBreakerCollectorTests extends ESSingleNodeTestCase {
+    private static final String TEST_INDEX = "test";
+    private CircuitBreakerCollector collector;
+    private long startTimeInMills = 1153721339;
+
+    @Before
+    public void init() {
+        IndicesService indicesService = getInstanceFromNode(IndicesService.class);
+        ESResources.INSTANCE.setIndicesService(indicesService);
+        ESResources.INSTANCE.setCircuitBreakerService(indicesService.getCircuitBreakerService());
+
+        MetricsConfiguration.CONFIG_MAP.put(CircuitBreakerCollector.class, MetricsConfiguration.cdefault);
+        collector = new CircuitBreakerCollector();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     @Test
-    public void testCircuitBreakerMetrics() {
-        MetricsConfiguration.CONFIG_MAP.put(CircuitBreakerCollector.class, MetricsConfiguration.cdefault);
-        System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
-        long startTimeInMills = 1153721339;
-        CircuitBreakerCollector circuitBreakerCollector = new CircuitBreakerCollector();
-        circuitBreakerCollector.saveMetricValues("werjbdsiviewur", startTimeInMills);
-        String fetchedValue = PerformanceAnalyzerMetrics.getMetric(PluginSettings.instance().getMetricsLocation()
-                + PerformanceAnalyzerMetrics.getTimeInterval(startTimeInMills)+"/circuit_breaker/");
-        PerformanceAnalyzerMetrics.removeMetrics(PluginSettings.instance().getMetricsLocation()
-                 + PerformanceAnalyzerMetrics.getTimeInterval(startTimeInMills));
-        assertEquals("werjbdsiviewur", fetchedValue);
+    public void testCollectMetrics() throws IOException {
+        createIndex(TEST_INDEX);
+        collector.collectMetrics(startTimeInMills);
+        List<CircuitBreakerStatus> metrics = readMetrics();
+        assertEquals(5, metrics.size());
+        assertEquals(CircuitBreaker.REQUEST, metrics.get(0).getType());
+        assertEquals(CircuitBreaker.FIELDDATA, metrics.get(1).getType());
+        assertEquals(CircuitBreaker.IN_FLIGHT_REQUESTS, metrics.get(2).getType());
+        assertEquals(CircuitBreaker.ACCOUNTING, metrics.get(3).getType());
+        assertEquals(CircuitBreaker.PARENT, metrics.get(4).getType());
+    }
 
-        try {
-            circuitBreakerCollector.saveMetricValues("12321.5464", startTimeInMills, "123");
-            assertEquals(true, true);
-        } catch (RuntimeException ex) {
-            //- expecting exception...1 values passed; 0 expected
+    private List<CircuitBreakerStatus> readMetrics() throws IOException {
+        List<Event> metrics = TestUtil.readEvents();
+        assert metrics.size() == 1;
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new ParanamerModule());
+        String[] jsonStrs = metrics.get(0).value.split("\n");
+        assert jsonStrs.length == 6;
+        List<CircuitBreakerStatus> list = new ArrayList<>();
+        for (int i = 1; i < 6; i++) {
+            list.add(objectMapper.readValue(jsonStrs[i], CircuitBreakerStatus.class));
         }
-
-        try {
-            circuitBreakerCollector.getMetricsPath(startTimeInMills, "123", "x");
-            assertEquals(true, true);
-        } catch (RuntimeException ex) {
-            //- expecting exception...2 values passed; 0 expected
-        }
+        return list;
     }
 }
