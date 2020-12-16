8b5022c47d3456d1db5da3060d231aaabf167b60
Yu Sun <sunayu@dev-dsk-sunayu-2b-daf3bb41.us-west-2.amazon.com>
2020-12-16 01:01:35 -0800

    add UT for PerformanceAnalyzerSearchListenerTests

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/listener/PerformanceAnalyzerSearchListenerTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/listener/PerformanceAnalyzerSearchListenerTests.java
@@ -15,33 +15,46 @@
 
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.listener;
 
+import static org.junit.Assert.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors.StatExceptionCode;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors.StatsCollector;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.config.PerformanceAnalyzerController;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.config.PluginSettings;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.jvm.ThreadList;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetrics;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsConfiguration;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.util.TestUtil;
 import org.apache.commons.lang3.SystemUtils;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.search.internal.SearchContext;
 import org.elasticsearch.search.internal.ShardSearchRequest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-@Ignore
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PerformanceAnalyzerSearchListenerTests {
   private static final String TEST_INDEX = "test";
+  private static final long TOOK_IN_NANOS = 10;
+  private static final String EXCEPTION = StatExceptionCode.ES_REQUEST_INTERCEPTOR_ERROR.toString();
 
-  private PerformanceAnalyzerSearchListener performanceAnalyzerSearchListener;
+  private PerformanceAnalyzerSearchListener searchListener;
+  private StatsCollector statsCollector;
+  private long startTimeInMills = 1253721339;
+  private final AtomicInteger errorCount = new AtomicInteger(0);
+
   @Mock private SearchContext searchContext;
   @Mock private ShardSearchRequest shardSearchRequest;
   @Mock private ShardId shardId;
-
-  @Mock
-  private PerformanceAnalyzerController controller;
-
+  @Mock private PerformanceAnalyzerController controller;
 
   @Before
   public void setup() {
@@ -51,22 +64,136 @@ public class PerformanceAnalyzerSearchListenerTests {
 
     initMocks(this);
     Mockito.when(controller.isPerformanceAnalyzerEnabled()).thenReturn(true);
-    Mockito.when(searchContext.request()).thenReturn(shardSearchRequest);
-    Mockito.when(shardSearchRequest.shardId()).thenReturn(shardId);
-    Mockito.when(shardId.getIndexName()).thenReturn("shardIndex");
-    Mockito.when(shardId.getId()).thenReturn(1);
 
     MetricsConfiguration.CONFIG_MAP.put(ThreadList.class, MetricsConfiguration.cdefault);
-    performanceAnalyzerSearchListener = new PerformanceAnalyzerSearchListener(controller);
+    searchListener = new PerformanceAnalyzerSearchListener(controller);
+    assertEquals(PerformanceAnalyzerSearchListener.class.getName(), searchListener.toString());
 
-//    String params[] = new String[0];
-//      ThreadList.runThreadDump(OSGlobals.getPid(), params);
-//      ThreadList.LOGGER.info(ThreadList.getNativeTidMap().values());
+    statsCollector = StatsCollector.instance();
+  }
+
+  @Test
+  public void testGetMetricsPath() {
+    String expectedPath = PluginSettings.instance().getMetricsLocation()
+            + PerformanceAnalyzerMetrics.getTimeInterval(startTimeInMills) + "/"
+            + PerformanceAnalyzerMetrics.sThreadsPath + "/" + "SearchThread" + "/"
+            + "ShardQuery" +"/"
+            + "ShardSearchID" + "/"
+            + PerformanceAnalyzerMetrics.FINISH_FILE_NAME;
+    String actualPath = searchListener.getMetricsPath(startTimeInMills, "SearchThread", "ShardQuery", "ShardSearchID", PerformanceAnalyzerMetrics.FINISH_FILE_NAME);
+    assertEquals(expectedPath, actualPath);
+
+    try {
+      searchListener.getMetricsPath(startTimeInMills, "SearchThread", "ShardQuery", "ShardSearchID");
+      fail("Negative scenario test: Should have been a RuntimeException");
+    } catch (RuntimeException ex) {
+      //- expecting exception...3 values passed; 4 expected
+    }
   }
 
   @Test
   public void testOnPreQueryPhase() {
-    performanceAnalyzerSearchListener.onPreQueryPhase(searchContext);
+    initializeValidSearchContext(true);
+    searchListener.onPreQueryPhase(searchContext);
+    List<String> jsonStrs = readMetricsInJsonString(4);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.START_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
   }
 
+  @Test
+  public void testOnQueryPhase() {
+    initializeValidSearchContext(true);
+    searchListener.onQueryPhase(searchContext, TOOK_IN_NANOS);
+    List<String> jsonStrs = readMetricsInJsonString(5);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.FINISH_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.FAILED.toString()));
+    assertTrue(jsonStrs.get(1).contains("false"));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(3).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
+  }
+
+
+  @Test
+  public void testOnFailedQueryPhase() {
+    initializeValidSearchContext(true);
+    searchListener.onFailedQueryPhase(searchContext);
+    List<String> jsonStrs = readMetricsInJsonString(5);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.FINISH_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.FAILED.toString()));
+    assertTrue(jsonStrs.get(1).contains("true"));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(3).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
+  }
+
+  @Test
+  public void testOnPreFetchPhase() {
+    initializeValidSearchContext(true);
+    searchListener.onPreFetchPhase(searchContext);
+    List<String> jsonStrs = readMetricsInJsonString(4);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.START_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
+  }
+
+  @Test
+  public void testOnFetchPhase() {
+    initializeValidSearchContext(true);
+    searchListener.onFetchPhase(searchContext, TOOK_IN_NANOS);
+    List<String> jsonStrs = readMetricsInJsonString(5);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.FINISH_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.FAILED.toString()));
+    assertTrue(jsonStrs.get(1).contains("false"));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(3).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
+  }
+
+  @Test
+  public void testOnFailedFetchPhase() {
+    initializeValidSearchContext(true);
+    searchListener.onFailedFetchPhase(searchContext);
+    List<String> jsonStrs = readMetricsInJsonString(5);
+    assertTrue(jsonStrs.get(0).contains(AllMetrics.CommonMetric.FINISH_TIME.toString()));
+    assertTrue(jsonStrs.get(1).contains(AllMetrics.CommonDimension.FAILED.toString()));
+    assertTrue(jsonStrs.get(1).contains("true"));
+    assertTrue(jsonStrs.get(2).contains(AllMetrics.CommonDimension.INDEX_NAME.toString()));
+    assertTrue(jsonStrs.get(3).contains(AllMetrics.CommonDimension.SHARD_ID.toString()));
+  }
+
+  private List<String> readMetricsInJsonString(int length) {
+    List<Event> metrics = TestUtil.readEvents();
+    assert metrics.size() == 1;
+    String[] jsonStrs = metrics.get(0).value.split("\n");
+    assert jsonStrs.length == length;
+    return Arrays.asList(jsonStrs).subList(1, jsonStrs.length);
+  }
+
+  @Test
+  public void testInvalidSearchContext() {
+    initializeValidSearchContext(false);
+
+    searchListener.onFailedFetchPhase(searchContext);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+    searchListener.onPreFetchPhase(searchContext);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+    searchListener.onFetchPhase(searchContext, TOOK_IN_NANOS);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+    searchListener.onPreQueryPhase(searchContext);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+    searchListener.onFailedQueryPhase(searchContext);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+    searchListener.onQueryPhase(searchContext, TOOK_IN_NANOS);
+    assertEquals(errorCount.incrementAndGet(), statsCollector.getCounters().get(EXCEPTION).intValue());
+  }
+
+  private void initializeValidSearchContext(boolean isValid) {
+    if (isValid) {
+      Mockito.when(searchContext.request()).thenReturn(shardSearchRequest);
+      Mockito.when(shardSearchRequest.shardId()).thenReturn(shardId);
+      Mockito.when(shardId.getIndexName()).thenReturn("shardIndex");
+      Mockito.when(shardId.getId()).thenReturn(1);
+    } else {
+      Mockito.when(searchContext.request()).thenReturn(null);
+    }
+  }
 }
