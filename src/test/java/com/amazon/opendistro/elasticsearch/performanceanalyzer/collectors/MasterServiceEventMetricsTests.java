2bc19bd8107ed8a4e3a80f9579a6ef5b7ff28a3f
Yu Sun <sunayu@amazon.com>
2020-12-17 11:31:23 -0800

    move linuxOS check to @BeforeClass

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/MasterServiceEventMetricsTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/MasterServiceEventMetricsTests.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright <2020> Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.amazon.opendistro.elasticsearch.performanceanalyzer.collectors;
 
-import static org.junit.Assert.*;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.ESResources;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.config.PluginSettings;
@@ -10,35 +26,35 @@ import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsCo
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.util.TestUtil;
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.lang3.SystemUtils;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.cluster.service.SourcePrioritizedRunnable;
 import org.elasticsearch.common.Priority;
 import org.elasticsearch.common.util.concurrent.PrioritizedEsThreadPoolExecutor;
 import org.elasticsearch.test.ClusterServiceUtils;
-import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.threadpool.TestThreadPool;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MasterServiceEventMetricsTests {
   private long startTimeInMills = 1153721339;
   private MasterServiceEventMetrics masterServiceEventMetrics;
   private ThreadPool threadPool;
 
-  @Before
-  public void init() {
+  @BeforeClass
+  public static void setup() {
     // this test only runs in Linux system
     // as some of the static members of the ThreadList class are specific to Linux
     org.junit.Assume.assumeTrue(SystemUtils.IS_OS_LINUX);
+  }
 
+  @Before
+  public void init() {
     threadPool = new TestThreadPool("test");
     ClusterService clusterService = ClusterServiceUtils.createClusterService(threadPool);
     ESResources.INSTANCE.setClusterService(clusterService);
@@ -48,7 +64,7 @@ public class MasterServiceEventMetricsTests {
   }
 
   @After
-  public void tearDown() throws Exception {
+  public void tearDown() {
     threadPool.shutdownNow();
   }
 
@@ -100,8 +116,6 @@ public class MasterServiceEventMetricsTests {
     };
 
     prioritizedEsThreadPoolExecutor.submit(runnable);
-    System.out.println("after submit: " + System.currentTimeMillis());
-    System.out.println("before collect: " + System.currentTimeMillis());
     masterServiceEventMetrics.collectMetrics(startTimeInMills);
     List<String> jsonStrs = readMetricsInJsonString();
     assertTrue(jsonStrs.get(0).contains(AllMetrics.MasterMetricDimensions.MASTER_TASK_PRIORITY.toString()));
