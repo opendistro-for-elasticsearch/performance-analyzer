3f999231893380e88592cb7b1ed486741f129b74
Yu Sun <sunayu@amazon.com>
2021-01-14 15:45:31 -0800

    clean metricQueue before running every test

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/ThreadPoolMetricsCollectorTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/ThreadPoolMetricsCollectorTests.java
@@ -51,6 +51,9 @@ public class ThreadPoolMetricsCollectorTests extends CustomMetricsLocationTestBa
         System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
         MetricsConfiguration.CONFIG_MAP.put(ThreadPoolMetricsCollector.class, MetricsConfiguration.cdefault);
         threadPoolMetricsCollector = new ThreadPoolMetricsCollector();
+
+        //clean metricQueue before running every test
+        TestUtil.readEvents();
     }
 
     @Test
