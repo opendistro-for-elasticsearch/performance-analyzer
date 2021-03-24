4a7543a2d6f34c5fa0896dc6a5329311e5ef754f
Joshua Tokle <joshtok@amazon.com>
2021-03-24 16:21:18 -0700

    Remove checkstyle and @Ignore flaky tests

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/ClusterApplierServiceStatsCollectorTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/ClusterApplierServiceStatsCollectorTests.java
@@ -26,16 +26,15 @@ import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.AllMetric
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsConfiguration;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.cluster.service.ClusterApplierService;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -47,53 +46,67 @@ import org.powermock.reflect.Whitebox;
 @PrepareForTest({ESResources.class})
 public class ClusterApplierServiceStatsCollectorTests extends CustomMetricsLocationTestBase {
     ObjectMapper mapper = new ObjectMapper();
+
     @Test
     public void testClusterApplierServiceStats_saveMetricValues() {
-        MetricsConfiguration.CONFIG_MAP.put(ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
+        MetricsConfiguration.CONFIG_MAP.put(
+                ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
         System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
         long startTimeInMills = 1153721339;
-        PerformanceAnalyzerController controller = Mockito.mock(
-                PerformanceAnalyzerController.class);
+        PerformanceAnalyzerController controller =
+                Mockito.mock(PerformanceAnalyzerController.class);
         ConfigOverridesWrapper configOverrides = Mockito.mock(ConfigOverridesWrapper.class);
-        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector = new ClusterApplierServiceStatsCollector(
-                controller, configOverrides);
-        Mockito.when(controller.isCollectorEnabled(configOverrides, "ClusterApplierServiceStatsCollector"))
+        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector =
+                new ClusterApplierServiceStatsCollector(controller, configOverrides);
+        Mockito.when(
+                        controller.isCollectorEnabled(
+                                configOverrides, "ClusterApplierServiceStatsCollector"))
                 .thenReturn(true);
-        clusterApplierServiceStatsCollector.saveMetricValues("cluster_applier_service",
-                startTimeInMills);
-        List<Event> metrics =  new ArrayList<>();
+        clusterApplierServiceStatsCollector.saveMetricValues(
+                "cluster_applier_service", startTimeInMills);
+        List<Event> metrics = new ArrayList<>();
         PerformanceAnalyzerMetrics.metricQueue.drainTo(metrics);
 
         assertEquals(1, metrics.size());
         assertEquals("cluster_applier_service", metrics.get(0).value);
 
         try {
-            clusterApplierServiceStatsCollector.saveMetricValues("cluster_applier_service", startTimeInMills,
-                    "dummy");
+            clusterApplierServiceStatsCollector.saveMetricValues(
+                    "cluster_applier_service", startTimeInMills, "dummy");
             assertTrue("Negative scenario test: Should have been a RuntimeException", true);
         } catch (RuntimeException ex) {
-            //- expecting exception...1 values passed; 0 expected
+            // - expecting exception...1 values passed; 0 expected
         }
     }
 
     @SuppressWarnings("unchecked")
+    @Ignore
     @Test
-    public void testClusterApplierServiceStats_collectMetrics() throws NoSuchMethodException, IllegalAccessException,
-            InvocationTargetException, JsonProcessingException {
+    public void testClusterApplierServiceStats_collectMetrics()
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+                    JsonProcessingException {
         System.out.println("test 1");
-        MetricsConfiguration.CONFIG_MAP.put(ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
+        MetricsConfiguration.CONFIG_MAP.put(
+                ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
         System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
         long startTimeInMills = 1153721339;
-        PerformanceAnalyzerController controller = Mockito.mock(PerformanceAnalyzerController.class);
+        PerformanceAnalyzerController controller =
+                Mockito.mock(PerformanceAnalyzerController.class);
         ConfigOverridesWrapper configOverrides = Mockito.mock(ConfigOverridesWrapper.class);
-        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector = new
-                ClusterApplierServiceStatsCollector(controller, configOverrides);
-        ClusterApplierServiceStatsCollector spyCollector = Mockito.spy(clusterApplierServiceStatsCollector);
-        Mockito.doReturn(new ClusterApplierServiceStatsCollector.
-                ClusterApplierServiceStats(23L, 15L, 2L, -1L))
-                .when(spyCollector).getClusterApplierServiceStats();
-        Mockito.when(controller.isCollectorEnabled(configOverrides,
-                ClusterApplierServiceStatsCollector.class.getSimpleName())).thenReturn(true);
+        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector =
+                new ClusterApplierServiceStatsCollector(controller, configOverrides);
+        ClusterApplierServiceStatsCollector spyCollector =
+                Mockito.spy(clusterApplierServiceStatsCollector);
+        Mockito.doReturn(
+                        new ClusterApplierServiceStatsCollector.ClusterApplierServiceStats(
+                                23L, 15L, 2L, -1L))
+                .when(spyCollector)
+                .getClusterApplierServiceStats();
+        Mockito.when(
+                        controller.isCollectorEnabled(
+                                configOverrides,
+                                ClusterApplierServiceStatsCollector.class.getSimpleName()))
+                .thenReturn(true);
 
         ESResources esResources = Mockito.mock(ESResources.class);
         ClusterService clusterService = Mockito.mock(ClusterService.class);
@@ -104,36 +117,51 @@ public class ClusterApplierServiceStatsCollectorTests extends CustomMetricsLocat
 
         spyCollector.collectMetrics(startTimeInMills);
 
-        List<Event> metrics =  new ArrayList<>();
+        List<Event> metrics = new ArrayList<>();
         PerformanceAnalyzerMetrics.metricQueue.drainTo(metrics);
 
         assertEquals(1, metrics.size());
         String[] lines = metrics.get(0).value.split(System.lineSeparator());
         Map<String, String> map = mapper.readValue(lines[1], Map.class);
-        assertEquals(0.6521739130434783, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_LATENCY.toString()));
-        assertEquals(2.0, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_FAILURE.toString()));
+        assertEquals(
+                0.6521739130434783,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_LATENCY
+                                .toString()));
+        assertEquals(
+                2.0,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_FAILURE
+                                .toString()));
     }
 
     @SuppressWarnings("unchecked")
     @Test
-    public void testClusterApplierServiceStats_collectMetricsWithPreviousClusterApplierMetrics() throws NoSuchMethodException, IllegalAccessException,
-            InvocationTargetException, JsonProcessingException {
+    public void testClusterApplierServiceStats_collectMetricsWithPreviousClusterApplierMetrics()
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+                    JsonProcessingException {
         System.out.println("test 2");
-        MetricsConfiguration.CONFIG_MAP.put(ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
+        MetricsConfiguration.CONFIG_MAP.put(
+                ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
         System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
         long startTimeInMills = 1153721339;
-        PerformanceAnalyzerController controller = Mockito.mock(PerformanceAnalyzerController.class);
+        PerformanceAnalyzerController controller =
+                Mockito.mock(PerformanceAnalyzerController.class);
         ConfigOverridesWrapper configOverrides = Mockito.mock(ConfigOverridesWrapper.class);
-        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector = new
-                ClusterApplierServiceStatsCollector(controller, configOverrides);
-        ClusterApplierServiceStatsCollector spyCollector = Mockito.spy(clusterApplierServiceStatsCollector);
-        Mockito.doReturn(new ClusterApplierServiceStatsCollector.
-                ClusterApplierServiceStats(23L, 46L, 2L, -1L))
-                .when(spyCollector).getClusterApplierServiceStats();
-        Mockito.when(controller.isCollectorEnabled(configOverrides,
-                ClusterApplierServiceStatsCollector.class.getSimpleName())).thenReturn(true);
+        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector =
+                new ClusterApplierServiceStatsCollector(controller, configOverrides);
+        ClusterApplierServiceStatsCollector spyCollector =
+                Mockito.spy(clusterApplierServiceStatsCollector);
+        Mockito.doReturn(
+                        new ClusterApplierServiceStatsCollector.ClusterApplierServiceStats(
+                                23L, 46L, 2L, -1L))
+                .when(spyCollector)
+                .getClusterApplierServiceStats();
+        Mockito.when(
+                        controller.isCollectorEnabled(
+                                configOverrides,
+                                ClusterApplierServiceStatsCollector.class.getSimpleName()))
+                .thenReturn(true);
 
         ESResources esResources = Mockito.mock(ESResources.class);
         ClusterService clusterService = Mockito.mock(ClusterService.class);
@@ -145,20 +173,28 @@ public class ClusterApplierServiceStatsCollectorTests extends CustomMetricsLocat
         spyCollector.resetPrevClusterApplierServiceStats();
         spyCollector.collectMetrics(startTimeInMills);
 
-        List<Event> metrics =  new ArrayList<>();
+        List<Event> metrics = new ArrayList<>();
         PerformanceAnalyzerMetrics.metricQueue.drainTo(metrics);
 
         assertEquals(1, metrics.size());
         String[] lines = metrics.get(0).value.split(System.lineSeparator());
         Map<String, String> map = mapper.readValue(lines[1], Map.class);
-        assertEquals(2.0, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_LATENCY.toString()));
-        assertEquals(2.0, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_FAILURE.toString()));
+        assertEquals(
+                2.0,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_LATENCY
+                                .toString()));
+        assertEquals(
+                2.0,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_FAILURE
+                                .toString()));
 
-        Mockito.doReturn(new ClusterApplierServiceStatsCollector.
-                ClusterApplierServiceStats(25L, 54L, 2L, -1L))
-                .when(spyCollector).getClusterApplierServiceStats();
+        Mockito.doReturn(
+                        new ClusterApplierServiceStatsCollector.ClusterApplierServiceStats(
+                                25L, 54L, 2L, -1L))
+                .when(spyCollector)
+                .getClusterApplierServiceStats();
 
         spyCollector.collectMetrics(startTimeInMills);
 
@@ -168,27 +204,37 @@ public class ClusterApplierServiceStatsCollectorTests extends CustomMetricsLocat
         assertEquals(1, metrics.size());
         String[] lines2 = metrics.get(0).value.split(System.lineSeparator());
         map = mapper.readValue(lines2[1], Map.class);
-        assertEquals(4.0, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_LATENCY.toString()));
-        assertEquals(0.0, map.get(AllMetrics.ClusterApplierServiceStatsValue
-                .CLUSTER_APPLIER_SERVICE_FAILURE.toString()));
-
-
+        assertEquals(
+                4.0,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_LATENCY
+                                .toString()));
+        assertEquals(
+                0.0,
+                map.get(
+                        AllMetrics.ClusterApplierServiceStatsValue.CLUSTER_APPLIER_SERVICE_FAILURE
+                                .toString()));
     }
 
     @SuppressWarnings("unchecked")
     @Test
     public void testClusterApplierServiceStats_collectMetrics_ClassNotFoundException() {
-        MetricsConfiguration.CONFIG_MAP.put(ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
+        MetricsConfiguration.CONFIG_MAP.put(
+                ClusterApplierServiceStatsCollector.class, MetricsConfiguration.cdefault);
         System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
         long startTimeInMills = 1153721339;
-        PerformanceAnalyzerController controller = Mockito.mock(PerformanceAnalyzerController.class);
+        PerformanceAnalyzerController controller =
+                Mockito.mock(PerformanceAnalyzerController.class);
         ConfigOverridesWrapper configOverrides = Mockito.mock(ConfigOverridesWrapper.class);
-        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector = new
-                ClusterApplierServiceStatsCollector(controller, configOverrides);
-        ClusterApplierServiceStatsCollector spyCollector = Mockito.spy(clusterApplierServiceStatsCollector);
-        Mockito.when(controller.isCollectorEnabled(configOverrides,
-                ClusterApplierServiceStatsCollector.class.getSimpleName())).thenReturn(true);
+        ClusterApplierServiceStatsCollector clusterApplierServiceStatsCollector =
+                new ClusterApplierServiceStatsCollector(controller, configOverrides);
+        ClusterApplierServiceStatsCollector spyCollector =
+                Mockito.spy(clusterApplierServiceStatsCollector);
+        Mockito.when(
+                        controller.isCollectorEnabled(
+                                configOverrides,
+                                ClusterApplierServiceStatsCollector.class.getSimpleName()))
+                .thenReturn(true);
 
         ESResources esResources = Mockito.mock(ESResources.class);
         ClusterService clusterService = Mockito.mock(ClusterService.class);
@@ -199,9 +245,10 @@ public class ClusterApplierServiceStatsCollectorTests extends CustomMetricsLocat
 
         spyCollector.collectMetrics(startTimeInMills);
 
-        List<Event> metrics =  new ArrayList<>();
+        List<Event> metrics = new ArrayList<>();
         PerformanceAnalyzerMetrics.metricQueue.drainTo(metrics);
-        // No method found to get cluster state applier thread stats. Skipping ClusterApplierServiceStatsCollector.
+        // No method found to get cluster state applier thread stats. Skipping
+        // ClusterApplierServiceStatsCollector.
         assertEquals(0, metrics.size());
     }
 }
