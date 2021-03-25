28aa797c51a82526b10eb9355724aacdf052b02e
Mital Awachat <awachatm@amazon.com>
2021-03-25 11:28:56 +0530

    AdmissionControl: Clear metricQueue before running test

--- src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/AdmissionControlMetricsCollectorTests.java
+++ src/test/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/collectors/AdmissionControlMetricsCollectorTests.java
@@ -19,16 +19,23 @@ import static org.junit.Assert.assertEquals;
 
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.CustomMetricsLocationTestBase;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.MetricsConfiguration;
-import com.amazon.opendistro.elasticsearch.performanceanalyzer.metrics.PerformanceAnalyzerMetrics;
 import com.amazon.opendistro.elasticsearch.performanceanalyzer.reader_writer_shared.Event;
-import java.util.ArrayList;
+import com.amazon.opendistro.elasticsearch.performanceanalyzer.util.TestUtil;
 import java.util.List;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AdmissionControlMetricsCollectorTests extends CustomMetricsLocationTestBase {
 
-    @Ignore
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        // clean metricQueue before running every test
+        TestUtil.readEvents();
+        System.setProperty("performanceanalyzer.metrics.log.enabled", "False");
+    }
+
     @Test
     public void admissionControlMetricsCollector() {
         MetricsConfiguration.CONFIG_MAP.put(
@@ -39,8 +46,7 @@ public class AdmissionControlMetricsCollectorTests extends CustomMetricsLocation
         long startTimeInMills = System.currentTimeMillis();
         admissionControlMetricsCollector.saveMetricValues("testMetric", startTimeInMills);
 
-        List<Event> metrics = new ArrayList<>();
-        PerformanceAnalyzerMetrics.metricQueue.drainTo(metrics);
+        List<Event> metrics = TestUtil.readEvents();
         assertEquals(1, metrics.size());
         assertEquals("testMetric", metrics.get(0).value);
     }
